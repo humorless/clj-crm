f8634f1a7462fda2739b226392c16e6ad4cabb56
Laurence Chen <humorless@gmail.com>
2019-02-21 10:32:27 +0000

    add multi-method design into domain.query

--- src/clj/clj_crm/domain/query.clj
+++ src/clj/clj_crm/domain/query.clj
@@ -4,6 +4,30 @@
             [clojure.tools.logging :as log]
             [datomic.api :as d]))
 
+(defn query-command-switch
+  "Input:
+   user-q is the form: {:user ggyy8@gmail.com
+                         :q   \"all-customers \"}
+   Output:
+   :all-customers"
+  [user-q]
+  (keyword (:q user-q)))
+
+;; example usage: (dispatch-q {:user "ggyy8@gmail.com" :q "all-customers"}))
+(defmulti dispatch-q query-command-switch)
+
+(defmethod dispatch-q :all-customers
+  [user-q]
+  [:customer1
+   :customer2
+   :customer3])
+
+(defmethod dispatch-q :my-customers
+  [user-q]
+  (log/info "user-q as" user-q)
+  [:customer5
+   :customer9])
+
 (s/defschema Query {:q s/Str})
 
 (defn query
@@ -17,4 +41,7 @@
   [queries user req]
   (log/info "queries as" queries)
   (log/info "user as" user)
-  [:ok "Hello world"])
+  ;; check user queries
+  (let [uid (:user user)
+        user-queries (map #(assoc % :user uid) queries)]
+    [:ok (mapv dispatch-q user-queries)]))

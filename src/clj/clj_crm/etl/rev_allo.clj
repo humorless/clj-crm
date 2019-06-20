39974764f6a3c7eca8a7762ea703071aa36717e3
Laurence Chen <humorless@gmail.com>
2019-06-20 15:10:10 +0800

    finished etl/rev-allo.clj & add etl_test/dev_rev_allo.xlsx for this ETL

--- src/clj/clj_crm/etl/rev_allo.clj
+++ src/clj/clj_crm/etl/rev_allo.clj
@@ -8,14 +8,15 @@
    [clojure.java.io :as io]
    [clojure.spec.alpha :as spec]))
 
-(spec/def ::writing-time inst?)
+(spec/def ::apply-time inst?)
 (spec/def ::customer-id string?)
 (spec/def ::lamp-customer-id string?)
 (spec/def ::sales-name string?)
+(spec/def ::source string?)
 
 (spec/def ::mapping
   (spec/keys :req-un
-             [::writing-time ::customer-id ::lamp-customer-id ::sales-name]))
+             [::apply-time ::customer-id ::lamp-customer-id ::sales-name ::source]))
 
 (defn- check-mappings [state]
   (if (every? #(spec/valid? ::mapping %) (:mappings state))
@@ -31,11 +32,49 @@
   (with-open [stream (io/input-stream (str addr filename))]
     (->> (spreadsheet/load-workbook stream)
          (spreadsheet/select-sheet "Sheet0")
-         (spreadsheet/select-columns {:A :writing-time
-                                      :B :customer-id
-                                      :C :lamp-customer-id
-                                      :E :sales-name})
+         (spreadsheet/select-columns {:A :apply-time
+                                      :B :source
+                                      :C :customer-id
+                                      :D :lamp-customer-id
+                                      :F :sales-name})
          rest)))
+
+(defn- sales-name->eid
+  [db]
+  (->> (d/q '[:find ?n ?e
+              :where [?e :user/name ?n]] db)
+       (into {})))
+
+(defn- lamp-customer-id->eid
+  [db]
+  (->> (d/q '[:find ?n ?e
+              :where [?e :customer/id ?n]] db)
+       (into {})))
+
+(defn- raw->tx
+  [s-table c-table raw-m]
+  (let [{sn :sales-name at :apply-time ci :customer-id
+         lci :lamp-customer-id s :source} raw-m
+        s-eid (get s-table sn)
+        c-eid (get c-table lci)
+        s-keyword (keyword "etl.source" s)]
+    (when (nil? c-eid)
+      (log/error :lamp-customer-id lci  "has no mapping in database")
+      (throw (ex-info "raw data error" {:causes raw-m
+                                        :desc "lookup :rev-allo/customer failed"})))
+    (when (nil? s-eid)
+      (log/error :sales-name sn  "has no mapping in database")
+      (throw (ex-info "raw data error" {:causes raw-m
+                                        :desc "lookup :rev-allo/sales failed"})))
+    (when (and (not= s-keyword :etl.source/lap) (not= s-keyword :etl.source/agp))
+      (log/error :source s "source str error")
+      (throw (ex-info "raw data error" {:causes raw-m
+                                        :desc "convert source string to valid keyword failed"})))
+    {:rev-allo/sales s-eid
+     :rev-allo/customer c-eid
+     :rev-allo/customer-id ci
+     :rev-allo/time at
+     :rev-allo/source s-keyword}))
 
 (defn- rev-allo-raw->rev-allo-txes
   "The transformation:
@@ -45,10 +84,16 @@
   Store the `customer-id` using db.type/string
   Store the `time` using db.type/inst
   Store the `source` using db.type/keyword"
-  [db raw-map src])
+  [db state]
+  (let [s-table (sales-name->eid db)
+        c-table (lamp-customer-id->eid db)]
+    (->> (:mappings state)
+         (map #(raw->tx s-table c-table %)))))
 
 ;; Action with exception throwing
-(defn- get-excel [addr filename]
+
+
+(defn- get-excel-and-validate [addr filename]
   (-> {}
       (assoc :mappings (get-mappings-from-excel addr filename))
       (check-mappings)))
@@ -57,12 +102,11 @@
 (defn sync-data
   "read excel, database, and sync
    Assembly function"
-  [url filename src]
+  [url filename]
   (log/info "sync-data triggered!")
-  (let [raw-data-map (get-excel url filename)
+  (let [state (get-excel-and-validate url filename)
         db (d/db conn)
-        tx-retract-data (rev-allo-retract-txes db src)
-        tx-data (rev-allo-raw->rev-allo-txes db raw-data-map src)]
+        tx-data (rev-allo-raw->rev-allo-txes db state)]
     (do (log/info "tx-data write into db, length: " (count tx-data))
         (log/info "first item of tx-data" (first tx-data))
         (when (seq tx-data)

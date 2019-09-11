8e6c2e361b842dd55c9371863a42af264fad9191
Laurence Chen <humorless@gmail.com>
2019-09-11 15:28:16 +0800

    Improve db filter to filter out all the rev-stream eids which are not this quarter

--- src/clj/clj_crm/pc/core.clj
+++ src/clj/clj_crm/pc/core.clj
@@ -3,11 +3,12 @@
    [datomic.api :as d]
    [clojure.tools.logging :as log]
    [taoensso.nippy :as nippy]
+   [clojure.set :as cset]
    [clj-crm.fjr.core :as fjr]
    [clj-crm.config :refer [env]]
    [clj-crm.db.user :as duser]
    [clj-crm.db.revenue :as drevenue]
-   [clj-crm.db.core :as dcore]))
+   [clj-crm.db.core :as dcore :refer [conn]]))
 
 (defn delete-db []
   (d/delete-database (:auxi-database-url env)))
@@ -41,20 +42,46 @@
       v
       (nippy/thaw v))))
 
-(defn time-span-filter-fn
-  [time-span o-t-e r-t-e]
+(defn- all-rev-stream-eids [db]
+  (into #{}
+        (d/q '[:find [?e ...]
+               :where [?e :rev-stream/accounting-time]]
+             db)))
+
+(defn- this-time-span-rev-stream-eids [db time-span]
+  (into #{}
+        (d/q '[:find [?e ...]
+               :in $ [?t ...]
+               :where [?e :rev-stream/accounting-time ?t]]
+             db time-span)))
+
+;; How to verify
+;; (not-this-rev-stream-eids (d/db conn) #{"2019-02" "2019-03"})
+
+(defn- not-this-rev-stream-eids [db time-span]
+  (let [eids-s1 (all-rev-stream-eids db)
+        eids-s2 (this-time-span-rev-stream-eids db time-span)]
+    (cset/difference eids-s1 eids-s2)))
+
+(defn- time-span-filter-fn
+  [discard-eids]
   (fn [_  datom]
     (cond
-      (and (= o-t-e (.a datom)) (not (contains? time-span (.v datom)))) false
-      (and (= r-t-e (.a datom)) (not (contains? time-span (.v datom)))) false
+      (contains? discard-eids (.e datom)) false
       :else true)))
+
+(defn- db-exclude-rev-stream-outside-time-span
+  [db time-span]
+  (let [discard-eids (not-this-rev-stream-eids db time-span)
+        t-f (time-span-filter-fn discard-eids)]
+    (d/filter db t-f)))
+
+;; How to verify
+;; (seq (d/datoms filtered-db :aevt :rev-stream/accounting-time))
 
 (defn all-compute-and-store
   [tx db* time-span]
-  (let [order-time-eid (d/entid db* :accounting/month)
-        revenue-time-eid (d/entid db* :rev-stream/accounting-time)
-        t-f (time-span-filter-fn time-span order-time-eid revenue-time-eid)
-        db (d/filter db* t-f)]
+  (let [db (db-exclude-rev-stream-outside-time-span db* time-span)]
     (let [eids (duser/sales-eids db)
           {other-stream-ru-tuples :stream other-order-ru-tuples :order} (drevenue/u-eids->other-ru-tuples db eids)
           stream-ru-tuples (mapcat #(drevenue/u-eid->stream-ru-tuples db %) eids)
@@ -67,10 +94,7 @@
 
 (defn my-compute-and-store
   [tx db* time-span user-lookup-ref teamName]
-  (let [order-time-eid (d/entid db* :accounting/month)
-        revenue-time-eid (d/entid db* :rev-stream/accounting-time)
-        t-f (time-span-filter-fn time-span order-time-eid revenue-time-eid)
-        db (d/filter db* t-f)]
+  (let [db (db-exclude-rev-stream-outside-time-span db* time-span)]
     (let [u-eids (duser/u-eid->same-team-u-eids db user-lookup-ref)
           stream-ru-tuples (mapcat #(drevenue/u-eid->stream-ru-tuples db %) u-eids)
           order-ru-tuples (mapcat #(drevenue/u-eid->order-ru-tuples db %) u-eids)
